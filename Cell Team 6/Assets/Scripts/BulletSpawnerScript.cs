81eef88b6a16b8bdda20d509cf62dd64554a0218
Ben Shackman <Bshackman@protonmail.com>
2019-09-10 08:59:50 -0400

    Basic Object Pooling implimented

---
+++ Cell Team 6/Assets/Scripts/BulletSpawnerScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawnerScript : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        ObjectQueue.Instance.SpawnFromPool("SphereBullet", transform.position, Quaternion.identity);
+    }
+}
